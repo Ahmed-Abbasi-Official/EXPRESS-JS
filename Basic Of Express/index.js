c7d6e4281f71ba72c0494ef6c594d46c2a7e9f3d
Ahmed0312006 <farhad.khi17@gmail.com>
2024-09-13 15:21:46 +0500

    commit

--- Basic Of Express/index.js
+++ Basic Of Express/index.js
@@ -1,83 +1,82 @@
 
 
 
-    // Routing 
+
+// Routing
 
 
 
-const express = require('express')
-const app = express()
-const port = 4000
 
-        // For Home Page Route .
+const express = require("express");
+const app = express();
+const port = 4000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Home</h1>')
-})
+// For Home Page Route .
 
-    // For About Page Route .
+app.get("/", (req, res) => {
+  res.send("<h1>Home</h1>");
+});
 
-app.get('/about', (req, res) => {
-  res.send('<h1>about</h1>')
-})
+// For About Page Route .
 
-    // For Contact Page Route .
+app.get("/about", (req, res) => {
+  res.send("<h1>about</h1>");
+});
 
-app.get('/contact', (req, res) => {
-  res.send('<h1>Contact</h1>')
-})
+// For Contact Page Route .
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact</h1>");
+});
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+  console.log(`http://localhost:${port}/`);
+});
 
 
 
 
-
-      // Middlewares
-
+// Middlewares
 
 
 
-        // For Home Page Route .
 
-app.use((req,res,next)=>{
+// For Home Page Route .
+
+app.use((req, res, next) => {
   console.log("me Middleware hu !!");
-  next()  // ye agar na hu tu ye req agly route pr nhy ja pae ga
-  
-})
-app.use((req,res,next)=>{
+  next(); // ye agar na hu tu ye req agly route pr nhy ja pae ga
+});
+app.use((req, res, next) => {
   console.log("me 2nd Middleware hu !!");
-  next()  // ye agar na hu tu ye req agly route pr nhy ja pae ga
-  
-})
+  next(); // ye agar na hu tu ye req agly route pr nhy ja pae ga
+});
 
-app.get('/', (req, res) => {
-  res.send('<h1>Home</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Home</h1>");
+});
 
-    // For About Page Route .
+// For About Page Route .
 
-app.get('/about', (req, res) => {
-  res.send('<h1>about</h1>')
-})
+app.get("/about", (req, res) => {
+  res.send("<h1>about</h1>");
+});
 
-    // For Contact Page Route .
+// For Contact Page Route .
 
-app.get('/contact', (req, res) => {
-  res.send('<h1>Contact</h1>')
-})
-
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact</h1>");
+});
 
 
 
 
-      //  Route Parameter
+
+//  Route Parameter
 
 
 
 
+app.get("/profile/:username", (req, res) => {
+  res.send(`<h1>hello ${req.params.username}</h1>`);
+});
