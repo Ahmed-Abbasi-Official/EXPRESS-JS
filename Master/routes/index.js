cffa1361f07b0e95cee0cfe2e7ba25072e6cf44f
Ahmed0312006 <farhad.khi17@gmail.com>
2024-09-17 15:27:13 +0500

    commit

--- Master/routes/index.js
+++ Master/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+                              // =================
+                              //  FLASH MESSAGES
+                              // =================
+
+
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/failed',(req,res)=>{
+  req.flash('age',12)
+  req.flash('name',"Ahmed")
+  res.send('bangya')
+})
+
+router.get('/check',(req,res)=>{
+  console.log(req.flash('age'),req.flash('name'));
+  res.send('check')
+  
+})
 
 module.exports = router;
