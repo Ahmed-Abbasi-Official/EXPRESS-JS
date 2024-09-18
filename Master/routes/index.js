0bc65a6aa0c94897b504489b7de785a948c332e1
Ahmed0312006 <farhad.khi17@gmail.com>
2024-09-18 19:50:52 +0500

    commit

--- Master/routes/index.js
+++ Master/routes/index.js
@@ -2,11 +2,13 @@ var express = require("express");
 var router = express.Router();
 const userModel = require("./users");
 
+
+
                                             // =================
                                             //  FLASH MESSAGES
                                             // =================
 
-                                            
+
 
 router.get("/", function (req, res, next) {
   res.render("index");
