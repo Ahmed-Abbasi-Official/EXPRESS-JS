6eb1e450c98f65e3f2e2630f14af5e6d8c802bf3
Ahmed0312006 <farhad.khi17@gmail.com>
2024-09-18 19:46:04 +0500

    commit

--- Master/routes/index.js
+++ Master/routes/index.js
@@ -1,75 +1,80 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const userModel=require('./users')
+const userModel = require("./users");
 
+// =================
+//  FLASH MESSAGES
+// =================
 
-                              // =================
-                              //  FLASH MESSAGES
-                              // =================
-
-
-
-
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get("/", function (req, res, next) {
+  res.render("index");
 });
 
-router.get('/failed',(req,res)=>{
-  req.flash('age',12)
-  req.flash('name',"Ahmed")
-  res.send('bangya')
-})
+router.get("/failed", (req, res) => {
+  req.flash("age", 12);
+  req.flash("name", "Ahmed");
+  res.send("bangya");
+});
 
-router.get('/check',(req,res)=>{
-  console.log(req.flash('age'),req.flash('name'));
-  res.send('check')
-  
-})
-
-
-
-
-                              // =====================
-                              //  MONGOOSE QUESTION
-                              // =====================
+router.get("/check", (req, res) => {
+  console.log(req.flash("age"), req.flash("name"));
+  res.send("check");
+});
 
 
 
 
-
-                              // ====================================================
-                              // Case In-Sensitive Search (1) --- use new regExp()  :
-                              // ====================================================
-
+                                              // =====================
+                                              //  MONGOOSE QUESTION
+                                              // =====================
 
 
-                              
 
-router.get('/createData',async(req,res)=>{
+
+
+                                              // ====================================================
+                                              // Case In-Sensitive Search (1) --- use new regExp()  :
+                                              // ====================================================
+
+
+
+                                              
+
+router.get("/createData", async (req, res) => {
   try {
-    const data=await userModel.create({
-      userName:"harshita",
-      nickName:"Abbasi",
-      name:"Hasnain Abbasi",
-      discription:"Hi ! I am Ahmed Abbasi a Student Developer .",
-      catogaries:['HTML5','CSS','JS','BootStrap','Tailwind','Firebase','React'],
-      
-    })
-    res.send(data)
+    const data = await userModel.create({
+      userName: "harshita",
+      nickName: "Abbasi",
+      name: "Hasnain Abbasi",
+      discription: "Hi ! I am Ahmed Abbasi a Student Developer .",
+      catogaries: [
+        "HTML5",
+        "CSS",
+        "JS",
+        "BootStrap",
+        "Tailwind",
+        "Firebase",
+        "React",
+      ],
+    });
+    res.send(data);
   } catch (error) {
-    console.log("Error in Creating Data" , error);
-    res.status(500).send('Error')
-    
+    console.log("Error in Creating Data", error);
+    res.status(500).send("Error");
   }
-})
+});
 
-router.get('/findData',async(req,res)=>{
-  let find = new RegExp('Ahmed','i')    // name batao  , insensitive
-  let find1 = new RegExp('harsh','i')    // name batao  , insensitive  (is my ye har os ko find kry ga jis me hars ata hu like harshi, harshita , hars)
-  let find2=new RegExp('^HarSh$','i')     // ye code jis me (^ start yaha se hu ) (end $ is pe ) perfect search . ab beshak search me upperCase dalo .
-  const allUser=await userModel.find({userName:find2})
-  res.send(allUser)
-})
-
+router.get("/findData", async (req, res) => {
+  try {
+    let find = new RegExp("Ahmed", "i"); // name batao  , insensitive
+    let find1 = new RegExp("harsh", "i"); // name batao  , insensitive  (is my ye har os ko find kry ga jis me hars ata hu like harshi, harshita , hars)
+    let find2 = new RegExp("^HarSh$", "i"); // ye code jis me (^ start yaha se hu ) (end $ is pe ) perfect search . ab beshak search me upperCase dalo .
+    const allUser = await userModel.find({ userName: find2 });
+    res.send(allUser);
+  } catch (error) {
+    console.log("Error in Finding : ", error);
+    res.status(500).send("Finding Error ");
+  }
+});
 
 module.exports = router;
